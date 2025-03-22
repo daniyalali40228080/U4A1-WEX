446a0b0ca31ab91715864f0bd4e3a28d90cc6bdb
daniyalali40228080 <40228080@hruc.ac.uk>
2025-03-22 15:15:51 +0000

    fixed stafflogin

--- staffLogin.cpp
+++ staffLogin.cpp
@@ -6,17 +6,18 @@ void experiencesMenu();
 void employerMenu();
 
 void staffLogin() {
-	string staffUsername, staffPassword;
+	string staffUsername, staffPassword; // Initalise Variables
 	cout << endl << "Enter staff username: ";
-	cin >> staffUsername;
+	cin >> staffUsername; // Wait for input
 	cout << "Enter staff password: ";
-	cin >> staffPassword;
+	cin >> staffPassword; // Wait for input
 
+	// Verify credentials are correct
 	if (staffUsername == "staff" && staffPassword == "staff") {
 		cout << endl << "Logged In Successfully!" << endl;
 		cout << endl << "Please select one of the following options:\n 1) Experiences Menu\n 2) Employer Menu" << endl << endl;
 		int option;
-		cin >> option;
+		cin >> option; // Wait for input
 
 		switch (option) {
 		case 1:
@@ -26,5 +27,8 @@ void staffLogin() {
 			employerMenu();
 			break;
 		}
+		default:
+			staffLogin();
+			break;
 	}
 };
