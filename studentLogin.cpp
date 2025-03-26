dabe4de18e8a8235176544c03b39f13c8345c114
daniyalali40228080 <40228080@hruc.ac.uk>
2025-03-26 12:25:14 +0000

    Update studentLogin.cpp

--- studentLogin.cpp
+++ studentLogin.cpp
@@ -16,22 +16,22 @@ void studentLogin() {
 	if (studentUsername == "student" && studentPassword == "student") {
 		cout << "Logged In Successfully!" << endl;
 
-		cout << "Select one of the following options:\n 1) Display all opportunities\n 2) Search for opportunity\n\nOption: "
+		cout << "Select one of the following options:\n 1) Display all opportunities\n 2) Search for opportunity\n\nOption: ";
 		int option;
 		cin >> option;
 
 		// Switch to redirect use to the selected option
-		switch(option) {
-			case 1:
-				displayOpportunities();
-				break;
-			case 2:
-				searchOpportunity();
-				break;
-			default:
-				cout << "Invalid Option! Returning to Menu..."
-				studentLogin();
-				break;
+		switch (option) {
+		case 1:
+			displayOpportunities();
+			break;
+		case 2:
+			searchOpportunity();
+			break;
+		default:
+			cout << "Invalid Option! Returning to Menu...";
+			studentLogin();
+			break;
 		}
 	}
 };
