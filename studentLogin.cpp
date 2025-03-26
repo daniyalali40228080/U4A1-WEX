78f941a3daae4a8aeaf277d2317a1e60e690ab47
daniyalali40228080 <40228080@hruc.ac.uk>
2025-03-26 12:57:15 +0000

    Update studentLogin.cpp

--- studentLogin.cpp
+++ studentLogin.cpp
@@ -5,33 +5,44 @@ using namespace std;
 void displayOpportunities();
 void searchOpportunity();
 
+int loginAttempts = 2;
+
 void studentLogin() {
-	string studentUsername, studentPassword; // Initalise Variables
-	cout << endl << "Enter student username: ";
-	cin >> studentUsername;
-	cout << "Enter student password: ";
-	cin >> studentPassword;
+	if (!loginAttempts == 0) {
+		string studentUsername, studentPassword; // Initalise Variables
+		cout << endl << "Enter student username: ";
+		cin >> studentUsername;
+		cout << "Enter student password: ";
+		cin >> studentPassword;
 
-	// Check if credentials are correct
-	if (studentUsername == "student" && studentPassword == "student") {
-		cout << "Logged In Successfully!" << endl;
+		// Check if credentials are correct
+		if (studentUsername == "student" && studentPassword == "student") {
+			cout << "Logged In Successfully!" << endl;
 
-		cout << "Select one of the following options:\n 1) Display all opportunities\n 2) Search for opportunity\n\nOption: ";
-		int option;
-		cin >> option;
+			cout << "Select one of the following options:\n 1) Display all opportunities\n 2) Search for opportunity\n\nOption: ";
+			int option;
+			cin >> option;
 
-		// Switch to redirect use to the selected option
-		switch (option) {
-		case 1:
-			displayOpportunities();
-			break;
-		case 2:
-			searchOpportunity();
-			break;
-		default:
-			cout << "Invalid Option! Returning to Menu...";
-			studentLogin();
-			break;
+			// Switch to redirect use to the selected option
+			switch (option) {
+			case 1:
+				displayOpportunities();
+				break;
+			case 2:
+				searchOpportunity();
+				break;
+			default:
+				cout << "Invalid Option! Returning to Menu...";
+				studentLogin();
+				break;
+			}
 		}
+		else {
+			cout << "Incorrect Password! Try again.";
+			studentLogin();
+		}
+	}
+	else {
+		cout << endl << "Exceed Maximum login attempts. Terminating program...";
 	}
 };
