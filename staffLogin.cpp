5a8a079408a0834e813de26f187eb74163d9eb2e
daniyalali40228080 <40228080@hruc.ac.uk>
2025-03-27 10:31:12 +0000

    Update staffLogin.cpp

--- staffLogin.cpp
+++ staffLogin.cpp
@@ -8,54 +8,53 @@ void displayOpportunities();
 void searchOpportunity();
 void welcome();
 
+int loginAttempts = 3; // Adjusted to 3 for clarity
+
 void staffLogin() {
-	string staffUsername, staffPassword; // Initalise Variables
-	cout << endl << "Enter staff username: ";
-	cin >> staffUsername; // Wait for input
-	cout << "Enter staff password: ";
+    string staffUsername, staffPassword; // Initialise Variables
 
-	char key;
-	while ((key = _getch()) != '\r') {  // Enter key ('\r') to submit
-		if (key == '\b') {  // Handle backspace
-			if (!staffPassword.empty()) {
-				staffPassword.pop_back();
-				cout << "\b \b";  // Remove '*' from screen
-			}
-		}
-		else {
-			staffPassword += key;
-			cout << '*';  // Show '*' instead of actual character
-		}
-	}
+    while (loginAttempts > 0) {
+        cout << endl << "Enter staff username: ";
+        cin >> staffUsername;
+        cout << "Enter staff password: ";
+        cin >> staffPassword;
 
-	
-	if (staffUsername == "staff" && staffPassword == "staff") {
-		// Ask to select an option
-		cout << endl << "Select one of the following options:\n 1) Add an Opportunity\n 2) Display all Opportunities\n 3) Search for an Opportunity\n\nOption: ";
-		int option;
-		cin >> option;
+        if (staffUsername == "employer" && staffPassword == "employer") {
+            // Ask to select an option
+            cout << "Select one of the following options:\n"
+                 << " 1) Add an Opportunity\n"
+                 << " 2) Display all Opportunities\n"
+                 << " 3) Search for an Opportunity\n\n"
+                 << "Option: ";
+            int option;
+            cin >> option;
 
-		// Cycle through each option until correct one found and redirect
-		switch (option) {
-		case 1:
-			addOpportunity();
-			break;
-		case 2:
-			displayOpportunities();
-			break;
-		case 3:
-			searchOpportunity();
-			break;
-		default:
-			// On wrong option return to previous menu
-			cout << "Invalid Option! Returning to Menu...";
-			welcome();
-			break;
-		}
-	}
-	else {
-			
-		cout << "Incorrect Credentials! Try again.";
-		staffLogin();
-	}
+            // Handle the selected option
+            switch (option) {
+                case 1:
+                    addOpportunity();
+                    break;
+                case 2:
+                    displayOpportunities();
+                    break;
+                case 3:
+                    searchOpportunity();
+                    break;
+                default:
+                    cout << "Invalid Option! Returning to Menu...\n";
+                    welcome();
+                    break;
+            }
+            return; // Exit after successful login and option handling
+        } else {
+            loginAttempts--;
+            if (loginAttempts > 0) {
+                cout << "Incorrect username or password! Try again.\n";
+                cout << "You have " << loginAttempts << " login attempts remaining.\n";
+            } else {
+                cout << "Too many failed attempts. Exiting program.\n";
+                return; // Exit after exceeding login attempts
+            }
+        }
+    }
 }
