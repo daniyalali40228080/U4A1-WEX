85c7c791641bbeb4e091d741e9d7aa4d4ff54c05
daniyalali40228080 <40228080@hruc.ac.uk>
2025-05-01 11:33:20 +0100

    Update loginAttempts

--- studentLogin.cpp
+++ studentLogin.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h> // Hide Password Module
 using namespace std;
 
 //Initialise external functions
@@ -6,38 +7,63 @@ void displayOpportunities();
 void searchOpportunity();
 void applyOpportunity();
 
+int studentLoginAttempts = 3; // Number of password attempts allowed
+
 void studentLogin() {
 
-	string studentUsername, studentPassword; // Initalise Variables
-	cout << endl << "Enter student username: ";
-	cin >> studentUsername;
-	cout << "Enter student password: ";
-	cin >> studentPassword;
+	while (studentLoginAttempts > 0) {
+		string studentUsername, studentPassword; // Initalise Variables
+		cout << endl << "Enter student username: ";
+		cin >> studentUsername;
+		cout << "Enter student password: ";
+		
+		char key;
+		while ((key = _getch()) != '\r') {  // Enter key ('\r') to submit
+			if (key == '\b') {  // Handle backspace
+				if (!studentPassword.empty()) {
+					studentPassword.pop_back();
+					cout << "\b \b";  // Remove '*' from screen
+				}
+			}
+			else {
+				studentPassword += key;
+				cout << '*';  // Show '*' instead of actual character
+			}
+		}
 
-	// Check if credentials are correct
-	if (studentUsername == "student" && studentPassword == "student") {
-		cout << "Logged In Successfully!" << endl;
 
-		cout << "Select one of the following options:\n 1) Display all opportunities\n 2) Apply for opportunity\n\nOption: ";
-		int option;
-		cin >> option;
+		// Check if credentials are correct
+		if (studentUsername == "student" && studentPassword == "student") {
+			cout << endl << "Logged In Successfully!" << endl;
 
-		// Switch to redirect use to the selected option
-		switch (option) {
-		case 1:
-			displayOpportunities();
-			break;
-		case 2:
-			applyOpportunity();
-			break;
-		default:
-			cout << "Invalid Option! Returning to Menu...";
+			cout << "Select one of the following options:\n 1) Display all opportunities\n 2) Apply for opportunity\n\nOption: ";
+			int option;
+			cin >> option;
+
+			// Switch to redirect use to the selected option
+			switch (option) {
+			case 1:
+				displayOpportunities();
+				break;
+			case 2:
+				applyOpportunity();
+				break;
+			default:
+				cout << "Invalid Option! Returning to Menu...";
+				studentLogin();
+				break;
+			}
+		}
+		else {
+			studentLoginAttempts--;
+
+			cout << endl << "Incorrect Username or Password! Try again.";
+			cout << endl << "You have " << studentLoginAttempts << " login attempts remaining." << endl;
 			studentLogin();
-			break;
 		}
 	}
-	else {
-		cout << "Incorrect Password! Try again.";
-		studentLogin();
+	if (studentLoginAttempts <= 0) {
+		cout << endl << endl << "Exceeded maximum amount of login attempts, terminating program...";
+		exit(0);
 	}
-};
+}
